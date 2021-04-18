b757e52ceeb1758335500ad48e10a74674af2454
cyberdas <46424157+cyberdas@users.noreply.github.com>
2021-04-18 16:00:59 +0300

    saving in chunks done

--- apachelogservice/logs/management/commands/parselogs.py
+++ apachelogservice/logs/management/commands/parselogs.py
@@ -23,34 +23,14 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         passed_url = options.get('url')
         validator = GivenUrlValidator()
-        validator(passed_url)
-        downloader = DownloadFile()
+        validator(passed_url) # валидация url
+        downloader = DownloadFile() # скачивание файла
         file_path, time = downloader(passed_url)
         self.stdout.write(self.style.SUCCESS(f'\nFile Downloaded Successfully, location: {file_path} \n'))
         self.stdout.write(self.style.SUCCESS(f'It took {time} seconds'))
-        parser = ApacheLogParser()
+        parser = ApacheLogParser() # парсинг и сохранение в бд
         parser.parse_all(file_path)
         self.stdout.write('Saved log data to DB')
         if options.get('delete'):
             os.remove(file_path)
             self.stdout.write('Deleted log file')
-        #parser = ApacheLogParser()
-        #parser.parse_all('D:\Dev\brandquad_test\apachelogservice\logs\logs_dir\access_4.log')
-        #parser.parse_all(file_path)
-        #self.stdout.write(self.style.SUCCESS(f'File {filepath} successfully parsed and saved to db')) 
-        #try:
-       #     d = DownLoadFile(url)  # скачиваем файл и сохраняем, название файла содержит текущую дату для избегания повторения
-       # except:
-       #     pass
-       # else:
-        #    self.stdout.write(self.style.SUCCESS("File Downloaded Successfully"))
-        # download file
-        # url = GivenUrlValidator(passed_url)
-        #print(f'Command: {options["url"]}')
-        #print('Second line')
-        #if len(options['url']) > 3:
-            #self.stdout.write(self.style.SUCCESS('We did it reddit'))
-        #else:
-            #raise CommandError("wrong url")
-        #url = options['url']
-        # print(url)
--- apachelogservice/logs/management/commands/save_log_to_db.py
+++ apachelogservice/logs/management/commands/save_log_to_db.py
@@ -2,19 +2,20 @@ import time
 from collections import defaultdict
 
 from apachelogs import LogParser
+from django.apps import apps
 from logging import getLogger
 
 from logs.models import Log
-# from logs.models import Log
-# from logs.model import Log
-# combined parser
-# parser = LogParser('%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"')
 
-# сначала работает без ThreadPoolExecutor
+
 logger = getLogger(__name__)
 
 
 class BulkCreateManager:
+    """
+    Когда количество добавленных в очередь объектов превышает chunk_size,
+    они добавляются в БД
+    """
     def __init__(self, chunk_size=4000):
         self._create_queues = defaultdict(list)
         self.chunk_size = chunk_size
@@ -30,8 +31,7 @@ class BulkCreateManager:
 
     def add(self, obj):
         """
-        Add an object to the queue to be created, and call bulk_create if we
-        have enough objs.
+        Добавить объект в очередь для добавления
         """
         model_class = type(obj)
         model_key = model_class._meta.label
@@ -40,10 +40,6 @@ class BulkCreateManager:
             self._commit(model_class)
 
     def done(self):
-        """
-        Always call this upon completion to make sure the final partial chunk
-        is saved.
-        """
         for model_name, objs in self._create_queues.items():
             if len(objs) > 0:
                 self._commit(apps.get_model(model_name))
@@ -60,8 +56,7 @@ class ApacheLogParser:
         t1 = time.time()
         logger.info("Starting saving file to db")
         with open(file_path, 'r') as f:
-            next(f)
-            count = 0 # add http method, path
+            next(f) # первая строка - пустая
             bulk_mng = BulkCreateManager()
             for entry in self.parser.parse_lines(f, ignore_invalid=True):
                 ip_address = entry.remote_host
@@ -82,23 +77,5 @@ class ApacheLogParser:
                     user_agent=user_agent,
                     referer=referer
                 ))
-            print(time.time() - t1)
-                #a = line
-                #print(line)
-                #print(a)
-                #entry = self.parser.parse(line)
-                #print(entry)
-
-if __name__ == '__main__':
-    parser = ApacheLogParser()
-    parser.parse_all(r'D:\Dev\brandquad_test\apachelogservice\logs\logs_dir\access.log')
-# D:\Dev\brandquad_test\apachelogservice\logs\logs_dir\access_4.log
-# Bulk create is my new friend.
-# If anyone has the same problem, don't insert rows one at a time. 
-# Each create() call is one insert statement. Instead, add the objects to a list, 
-# and then bulk_create(the_list).
-# before docker start create manage.py createsuperuser
-# error while saving log(....) to db
-# chunk -
-# когда chunk заполнился - bulk_create
-# обнуляем chunk
+            bulk_mng.done() # удостовериться что все данные добавились в бд
+            logger.info(f"It took {time.time() - t1} seconds")
